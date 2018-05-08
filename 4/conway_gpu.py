3c47a92911f1cbc62f992e82d2ca597e3c087beb
Brian Tuomanen <btuomanen@outlook.com>
2018-05-07 18:41:37 -0700

    Update conway_gpu.py

--- 4/conway_gpu.py
+++ 4/conway_gpu.py
@@ -21,34 +21,39 @@ ker = SourceModule("""
 
 #define _INDEX(x,y)  ( _XM(x)  + _YM(y) * _WIDTH )
 
-__global__ void conway_ker(int * out, int * in  )
+__device__ int nbrs(int x, int y, int * in)
 {
-   // x, y are the appropriate values for the cell covered by this thread
-   int x = (int) _X, y = (int) _Y;
-   
-   // count the number of neighbors around the current cell
-   int n = in[ _INDEX(x -1, y+1) ] + in[ _INDEX(x-1, y) ] + in[ _INDEX(x-1, y-1) ] \
+     return in[ _INDEX(x -1, y+1) ] + in[ _INDEX(x-1, y) ] + in[ _INDEX(x-1, y-1) ] \
                    + in[ _INDEX(x, y+1)] + in[_INDEX(x, y - 1)] \
                    + in[ _INDEX(x+1, y+1) ] + in[ _INDEX(x+1, y) ] + in[ _INDEX(x+1, y-1) ];
+}
+
+__global__ void conway_ker(int * lattice_out, int * lattice  )
+{
+   // x, y are the appropriate values for the cell covered by this thread
+   int x = _X, y = _Y;
+   
+   // count the number of neighbors around the current cell
+   int n = nbrs(x, y, lattice);
                    
     
     // if the current cell is alive, then determine if it lives or dies for the next generation.
-    if ( in[_INDEX(x,y)] == 1)
+    if ( lattice[_INDEX(x,y)] == 1)
        switch(n)
        {
           // if the cell is alive: it remains alive only if it has 2 or 3 neighbors.
           case 2:
-          case 3: out[_INDEX(x,y)] = 1;
+          case 3: lattice_out[_INDEX(x,y)] = 1;
                   break;
-          default: out[_INDEX(x,y)] = 0;                   
+          default: lattice_out[_INDEX(x,y)] = 0;                   
        }
-    else if( in[_INDEX(x,y)] == 0 )
+    else if( lattice[_INDEX(x,y)] == 0 )
          switch(n)
          {
             // a dead cell comes to life only if it has 3 neighbors that are alive.
-            case 3: out[_INDEX(x,y)] = 1;
+            case 3: lattice_out[_INDEX(x,y)] = 1;
                     break;
-            default: out[_INDEX(x,y)] = 0;         
+            default: lattice_out[_INDEX(x,y)] = 0;         
          }
          
 }
@@ -77,13 +82,10 @@ if __name__ == '__main__':
     lattice = np.int32( np.random.choice([1,0], N*N, p=[0.25, 0.75]).reshape(N, N) )
     lattice_gpu = gpuarray.to_gpu(lattice)
     
-    newLattice_gpu = gpuarray.empty_like(lattice_gpu)
- 
+    newLattice_gpu = gpuarray.empty_like(lattice_gpu)        
 
     fig, ax = plt.subplots()
     img = ax.imshow(lattice_gpu.get(), interpolation='nearest')
     ani = animation.FuncAnimation(fig, update_gpu, fargs=(img, newLattice_gpu, lattice_gpu, N, ) , interval=0, frames=100, save_count=100)    
      
     plt.show()
-     
-
