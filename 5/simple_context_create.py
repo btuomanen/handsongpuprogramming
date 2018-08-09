d2c211d17151d6ffce78077288aba482d9724a4e
Brian Tuomanen <btuomanen@outlook.com>
2018-08-08 17:54:39 -0700

    Create simple_context_create.py

---
+++ 5/simple_context_create.py
@@ -0,0 +1,11 @@
+import numpy as np
+from pycuda import gpuarray
+import pycuda.driver as drv
+
+dev = drv.Device(0)
+ctx = dev.make_context()
+
+x = gpuarray.to_gpu(np.float32([1,2,3]))
+print x.get()
+
+ctx.pop()
