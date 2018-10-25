87812926e7cee119c1b7a64f6d3450d17d26cdb5
Brian Tuomanen <btuomanen@outlook.com>
2018-10-24 18:58:09 -0700

    Update mandelbrot_ctypes.py

--- 10/mandelbrot_ctypes.py
+++ 10/mandelbrot_ctypes.py
@@ -6,35 +6,16 @@ from matplotlib import pyplot as plt
 import numpy as np
 from ctypes import *
 
-mandelbrot_dll = windll.LoadLibrary('./mandelbrot.dll')
-lm = mandelbrot_dll.launch_mandelbrot
-# launch_mandelbrot(float * lattice, float * mandelbrot_graph, int max_iters,
-# float upper_bound, int lattice_size)
-lm.argtypes = [POINTER(c_float), POINTER(c_float), c_int, c_float, c_int]
+mandelbrot_dll = CDLL('./mandelbrot.dll')
+mandel = mandelbrot_dll.launch_mandelbrot
+mandel.argtypes = [POINTER(c_float), POINTER(c_float), c_int, c_float, c_int]
 
 
-def gpu_mandelbrot(width, height, real_low, real_high, imag_low, imag_high, max_iters, upper_bound):
+def gpu_mandelbrot(breadth, low, high, max_iters, upper_bound):
 
-    # we set up our complex lattice as such
-    lattice = np.matrix(np.linspace(real_low, real_high, width), dtype=np.float32)
-    #lattice_im = np.matrix(np.linspace( imag_high, imag_low, height), dtype=np.float32) 
-    #lattice_im = lattice_im.transpose()
-    
+    lattice = np.linspace(low, high, breadth, dtype=np.float32)
     out = np.empty(shape=(lattice.size,lattice.size), dtype=np.float32)
-    
-    lm(lattice.ctypes.data_as(POINTER(c_float)), out.ctypes.data_as(POINTER(c_float)), c_int(max_iters), c_float(upper_bound), c_int(lattice.size) )  
-    
-    # copy complex lattice to the GPU
-    #mandelbrot_lattice_gpu = gpuarray.to_gpu(mandelbrot_lattice)
-
-    # allocate an empty array on the GPU
-    #mandelbrot_graph_gpu = gpuarray.empty(shape=mandelbrot_lattice.shape, dtype=np.float32)
-
-    #mandel_ker( mandelbrot_lattice_gpu, mandelbrot_graph_gpu, np.int32(max_iters), np.float32(upper_bound))
-              
-    #mandelbrot_graph = mandelbrot_graph_gpu.get()
-    
-    
+    mandel(lattice.ctypes.data_as(POINTER(c_float)), out.ctypes.data_as(POINTER(c_float)), c_int(max_iters), c_float(upper_bound), c_int(lattice.size) )  
     
     return out
 
@@ -42,7 +23,7 @@ def gpu_mandelbrot(width, height, real_low, real_high, imag_low, imag_high, max_
 if __name__ == '__main__':
 
     t1 = time()
-    mandel = gpu_mandelbrot(512,512,-2,2,-2,2,100, 2)
+    mandel = gpu_mandelbrot(512,-2,2,100, 2)
     t2 = time()
 
     mandel_time = t2 - t1
