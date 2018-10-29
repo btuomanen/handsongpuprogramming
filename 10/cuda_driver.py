360b365f7aa2501835b362e1b88126460f1f9285
Brian Tuomanen <btuomanen@outlook.com>
2018-10-28 17:38:27 -0700

    Update cuda_driver.py

--- 10/cuda_driver.py
+++ 10/cuda_driver.py
@@ -1,61 +1,80 @@
-from ctypes import *
-import sys
+from __future__ import division
+from time import time
+import matplotlib
+from matplotlib import pyplot as plt
+import numpy as np
+from cuda_driver import *
 
-if 'linux' in sys.platform:
-	cuda = CDLL('libcuda.so')
-elif 'win' in sys.platform:
-	cuda = CDLL('nvcuda.dll')
+def mandelbrot(breadth, low, high, max_iters, upper_bound):
+    cuInit(0)
 
-CUDA_ERRORS = {0 : 'CUDA_SUCCESS', 1 : 'CUDA_ERROR_INVALID_VALUE', 200 : 'CUDA_ERROR_INVALID_IMAGE', 201 : 'CUDA_ERROR_INVALID_CONTEXT ', 400 : 'CUDA_ERROR_INVALID_HANDLE' }
+    cnt = c_int(0)
+    cuDeviceGetCount(byref(cnt))
+    
+    if cnt.value == 0:
+        raise Exception('No GPU device found!')
+    
 
-cuInit = cuda.cuInit
-cuInit.argtypes = [c_uint]
-cuInit.restype = int
+    cuDevice = c_int(0)
+    cuDeviceGet(byref(cuDevice), 0)
+    
+    cuContext = c_void_p()
+    cuCtxCreate(byref(cuContext), 0, cuDevice)
 
-cuDeviceGetCount = cuda.cuDeviceGetCount
-cuDeviceGetCount.argtypes = [POINTER(c_int)]
-cuDeviceGetCount.restype = int
+    cuModule = c_void_p()
+    cuModuleLoad(byref(cuModule), c_char_p('./mandelbrot.ptx'))
 
-cuDeviceGet = cuda.cuDeviceGet
-cuDeviceGet.argtypes = [POINTER(c_int), c_int]
-cuDeviceGet.restype = int
+    lattice = np.linspace(low, high, breadth, dtype=np.float32)
+    lattice_c = lattice.ctypes.data_as(POINTER(c_float))
+    lattice_gpu = c_void_p(0)
+    
+    # Set up graph output for host.  Notice that this acts like a host-side "malloc", and we ca
+    graph = np.zeros(shape=(lattice.size, lattice.size), dtype=np.float32)
+    
+    cuMemAlloc(byref(lattice_gpu), c_size_t(lattice.size*sizeof(c_float)))
 
-cuCtxCreate = cuda.cuCtxCreate
-cuCtxCreate.argtypes = [c_void_p, c_uint, c_int]
-cuCtxCreate.restype = int
+    graph_gpu = c_void_p(0)
+    cuMemAlloc(byref(graph_gpu), c_size_t(lattice.size**2 * sizeof(c_float)))
 
-cuModuleLoad = cuda.cuModuleLoad
-cuModuleLoad.argtypes = [c_void_p, c_char_p]
-cuModuleLoad.restype  = int
+    cuMemcpyHtoD(lattice_gpu, lattice_c, c_size_t(lattice.size*sizeof(c_float)))
 
-cuCtxSynchronize = cuda.cuCtxSynchronize
-cuCtxSynchronize.argtypes = []
-cuCtxSynchronize.restype = int
+    mandel_ker = c_void_p(0)
+    cuModuleGetFunction(byref(mandel_ker), cuModule, c_char_p('mandelbrot_ker'))
 
-cuModuleGetFunction = cuda.cuModuleGetFunction
-cuModuleGetFunction.argtypes = [c_void_p, c_void_p, c_char_p ]
-cuModuleGetFunction.restype = int
+    max_iters = c_int(max_iters)
+    upper_bound_squared = c_float(upper_bound**2)
+    lattice_size = c_int(lattice.size)
 
-cuMemAlloc = cuda.cuMemAlloc
-cuMemAlloc.argtypes = [c_void_p, c_size_t]
-cuMemAlloc.restype = int
+    mandel_args0 = [lattice_gpu, graph_gpu, max_iters, upper_bound_squared, lattice_size ]
+    mandel_args = [c_void_p(addressof(x)) for x in mandel_args0]
+    mandel_params = (c_void_p * len(mandel_args))(*mandel_args)
 
-cuMemcpyHtoD = cuda.cuMemcpyHtoD 
-cuMemcpyHtoD.argtypes = [c_void_p, c_void_p, c_size_t]
-cuMemAlloc.restype = int
+    gridsize = int(np.ceil(lattice.size**2 / 32))
+    cuLaunchKernel(mandel_ker, gridsize, 1, 1, 32, 1, 1, 10000, None, mandel_params, None)
 
-cuMemcpyDtoH = cuda.cuMemcpyDtoH 
-cuMemcpyDtoH.argtypes = [c_void_p, c_void_p, c_size_t]
-cuMemcpyDtoH.restype = int
+    # synchronize context after kernel launch
+    cuCtxSynchronize()
 
-cuMemFree = cuda.cuMemFree
-cuMemFree.argtypes = [c_void_p] 
-cuMemFree.restype = int
+    
+    cuMemcpyDtoH( cast(graph.ctypes.data, c_void_p), graph_gpu,  c_size_t(lattice.size**2*sizeof(c_float)))
+    
+    cuMemFree(lattice_gpu)
+    cuMemFree(graph_gpu)
+    cuCtxDestroy(cuContext)
 
-cuLaunchKernel = cuda.cuLaunchKernel
-cuLaunchKernel.argtypes = [c_void_p, c_uint, c_uint, c_uint, c_uint, c_uint, c_uint, c_uint, c_void_p, c_void_p, c_void_p]
-cuLaunchKernel.restype = int
+    return graph
 
-cuCtxDestroy = cuda.cuCtxDestroy
-cuCtxDestroy.argtypes = [c_void_p]
-cuCtxDestroy.restype = int
+
+if __name__ == '__main__':
+
+    t1 = time()
+    mandel = mandelbrot(512,-2,2,256, 2)
+    t2 = time()
+
+    mandel_time = t2 - t1
+
+    print 'It took %s seconds to calculate the Mandelbrot graph.' % mandel_time
+    
+    fig = plt.figure(1)
+    plt.imshow(mandel, extent=(-2, 2, -2, 2))
+    plt.show()
